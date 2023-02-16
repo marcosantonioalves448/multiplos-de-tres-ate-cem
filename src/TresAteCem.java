a820763767bc4d6e1fd1a2d70cfa401abd4ed10b
Marcos Alves <marcosantonioalves448@gmail.com>
2023-02-16 00:50:44 -0300

    Finalisado o projeto que mostra todos os múltiplos de 3 até 100

--- src/TresAteCem.java
+++ src/TresAteCem.java
@@ -4,6 +4,13 @@ public class TresAteCem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int multiplo = 0;
+
+		while(multiplo <= 100 ) {
+System.out.println(multiplo);			
+multiplo+=3;
+		}
+		
 	}
 
 }
